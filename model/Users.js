1204e2cb0b75277007d3cf49f8e35da49fc1fd9f
Vitor Ferraz Varela <vitorfvarela@gmail.com>
2020-04-23 10:07:26 -0300

    Adicionado express e outras dependências

---
+++ model/Users.js
@@ -0,0 +1,6 @@
+const BaseModel = require('./BaseModel');
+export default class Users extends BaseModel { 
+    constructor() { 
+        super();
+    }
+}
---
+++ routes/users.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/:id", (req, res)=> { 
+    res.send(`Eu recebi o parametro ${req.params.id}`);
+});
+
+module.exports = router;
