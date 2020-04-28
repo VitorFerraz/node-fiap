0f0f4e63f6e46752ea5ea7487bed6248fe7a0c5f
Vitor Ferraz Varela <vitorfvarela@gmail.com>
2020-04-27 22:16:33 -0300

    adicionado teste integrado

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
-require('dotenv').config();
-
-const express = require('express');
-const bodyParser = require("body-parser");
-
-const routes = require('./routes');
+const app = require("./server");
 
 const port = process.env.PORT || 5000;
 
-const app = express();
-
-app.use(bodyParser.json());
-
-app.use(routes);
-
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
---
+++ routes/__tests__/auth.spec.js
@@ -0,0 +1,36 @@
+const supertest = require("supertest");
+const app = require("../../server");
+jest.mock("../../utils/createToken", () => () => "19mob");
+describe("/POST auth", () => {
+  it("should be response with token", (done) => {
+    supertest(app)
+      .post("/auth")
+      .send({ email: "vitorvarela3@gmail.com", password: "123456" })
+      .set("Accept", "application/json")
+      .end((error, response) => {
+        expect(response.body.token).toEqual("19mob");
+        expect(response.statusCode).toEqual(200);
+        done();
+      });
+  });
+
+  it("should be response with error", (done) => {
+    supertest(app)
+      .post("/auth")
+      .send({ email: "", password: "" })
+      .set("Accept", "application/json")
+      .expect(
+        401,
+        {
+          code: "not_authorized",
+          message: "Usuario não autorizado",
+        },
+        done
+      )
+      .end((error, response) => {
+        expect(response.statusCode).toEqual(401);
+        expect(response.body.message).toEqual("Usuario não autorizado");
+        done();
+      });
+  });
+});
